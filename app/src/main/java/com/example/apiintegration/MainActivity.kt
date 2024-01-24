28395b6fe334f8d5bad8567fb9cfda2c4b39a09a
israrKundi <israrikhere@gmail.com>
2024-01-24 20:53:57 +0500

    final commit, all code is working

--- app/src/main/java/com/example/apiintegration/MainActivity.kt
+++ app/src/main/java/com/example/apiintegration/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -28,11 +29,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.apiintegration.ui.theme.ApiIntegrationTheme
 import com.squareup.picasso.Picasso
 import retrofit2.Call
@@ -56,6 +61,8 @@ class MainActivity : ComponentActivity() {
         val retrofitData = retrofitBuilder.getProductData()
 
         retrofitData.enqueue(object : Callback<MyData?> {
+
+            // On Successful Response
             override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
                 var responseBody = response.body()
                 val productList = responseBody?.products
@@ -66,20 +73,9 @@ class MainActivity : ComponentActivity() {
                     }
                 }
 
-//                val collectDataInSB = StringBuilder()
-//
-//                if (productList != null) {
-//                    for(myData in productList){
-//                        collectDataInSB.append(myData.title + "  ")
-//                    }
-//                    }
-//
-//                setContent {
-//                    MainScreen(collectDataInSB.toString())
-//                }
-
                 }
 
+            // In Case Of Failure
             override fun onFailure(call: Call<MyData?>, t: Throwable) {
                 Log.d("Main Activity", "OnFailure: " + t.message)
 
@@ -104,22 +100,35 @@ fun ProductCard(product: Product) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
+            .background(Color.LightGray)
     ) {
 
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .background(Color.LightGray)
         ) {
 
 
 //            Image(painter = product.thumbnail, contentDescription = "product Image")
             Column(
                 modifier = Modifier
-                    .width(70.dp)
-                    .padding(16.dp)
+                    .width(120.dp) // Increase width
+                    .height(200.dp) // Increase height
+                    .padding(vertical = 50.dp, horizontal = 10.dp)
             ) {
-                Picasso.get().load(product.thumbnail)
+
+
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(product.thumbnail)
+                        .crossfade(true) // Enable smooth fade-in
+                        .build(),
+                    contentDescription = "Image description for accessibility",
+                    placeholder = painterResource(androidx.core.R.drawable.ic_call_decline), // Optional placeholder
+                    error = painterResource(androidx.core.R.drawable.ic_call_answer_low),
+                    modifier = Modifier.fillMaxSize()
+                )
             }
 
             Column(
@@ -129,9 +138,11 @@ fun ProductCard(product: Product) {
             ) {
                 Text(text = product.title, fontWeight = FontWeight.Bold, fontSize = 16.sp)
                 Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "$${product.description}", fontSize = 10.sp)
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = "Price: $${product.price}", fontSize = 12.sp)
                 Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "Rating: ${product.rating}", fontSize = 10.sp)
+                Text(text = "Rating: ${product.rating}", fontSize = 12.sp)
             }
 
         }
